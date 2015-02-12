3ca939d55ba93c34375a8c208eca235f81b7521f
nancyorgan <nancyorgan@gmail.com>
2015-02-12 17:06:30 -0500

    Added pseudorandom generator class.

--- main.py
+++ main.py
@@ -3,26 +3,41 @@
 ######################################################
 ########### card-flip memory game ####################
 
-# dependencies
-import random
-import numpy as np
+class PRNG(object):
+  def __init__(self, seed):
+    self.state = seed
 
+  def next(self):
+    x = self.state
+    x = x + 1
+    x = x << 8 | x >> 8
+    x = x * 997
+    x = x % 998
+    self.state = x
+    return self.state
 
-# initialize the board
-letters = map(chr, range(65, 91))
-board_width = 4
-board_length = 4
-board_letters = random.sample(letters, board_width*board_length/2)*2
-#print board_letters
-scrambled_board = random.sample(board_letters, len(board_letters))
-#print scrambled_board
+# todo: ask user for time
+# todo: convert time to integer
+# todo: pass PRNG integer to seed the random number generator
+prng = PRNG(0)
 
-def chunks(l, n):
-    n = max(1, n)
-    return [l[i:i + n] for i in range(0, len(l), n)]
+for i in range(10):
+  print prng.next()
 
-board = np.matrix(chunks(scrambled_board, board_width))
-print board
+# initialize the board with a make_board function
+#def chunks(l, n):
+#    n = max(1, n)
+#    return [l[i:i + n] for i in range(0, len(l), n)]
+
+#def card_front(board_width, board_length):
+#  letters = map(chr, range(65, 91))
+#  board_letters = random.sample(letters, board_width*board_length/2)*2
+#  scrambled_board = random.sample(board_letters, len(board_letters))
+#  board = np.matrix(chunks(scrambled_board, board_width))
+#  print board
+
+#myboard = card_front(4,4)
+#print myboard
 
 # select two cards
 
