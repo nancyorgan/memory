c3d8b7e67a58514c198d9285634b22fb26b3b841
nancyorgan <nancyorgan@gmail.com>
2015-02-20 14:42:55 -0500

    Card flipping functionality

--- memory_gui_test.py
+++ memory_gui_test.py
@@ -6,8 +6,6 @@ from cocos.actions import *
 from cocos.director import director
 from cocos.layer import base_layers
 
-import pyglet
-import cocos
 image_files = ['resources/cat.png', 'resources/rabbit.png', 'resources/deer.png',
                'resources/worm.png', 'resources/martha.png', 'resources/planet.png']
 blank_file = 'resources/blank.png'
@@ -16,67 +14,65 @@ class Cards(object):
     def __init__(self, image_file, blank_file):
         self.image = cocos.sprite.Sprite(pyglet.image.load(image_file))
         self.blank = cocos.sprite.Sprite(pyglet.image.load(blank_file))
-        self.unique_id = image_file
+        self.image_file = image_file
         self.image.opacity = 0
 
-
-class Deck(object):
-    def __init__(self, options, height, width):
+class Hand(object):
+    def __init__(self, image_files, height, width):
         self.height = height
         self.width = width
 
-        cards = random.sample(options, ((height*width)/2))*2
+        cards = random.sample(image_files, int((height*width)/2))*2
         shuffled = random.sample(cards, len(cards))
         self.shuffled = shuffled
 
+        sprite_size = 120
+
         x_position = []
         for i in range(width):
-            x_position.append(40 + (120/2) + 120*i + 20*i)
+            spacex = (640 - (sprite_size*width + 5*(width - 1)))/2
+            x_position.append(spacex + (sprite_size/2) + sprite_size*i + 5*i)
         x_position = [x for x in x_position for _ in range(height)]
-        self.x_position = x_position
 
         y_position = []
         for i in range(height):
-            y_position.append(40 + (120/2) + 120*i + 20*i)
+            spacey = (640 - (sprite_size*height + 5*(height - 1)))/2
+            y_position.append(spacey + (sprite_size/2) + sprite_size*i + 5*i)
         y_position = y_position*width
-        self.y_position = y_position
 
-        self.deck = zip(shuffled, x_position, y_position)
+        self.posxy = zip(x_position, y_position)
 
-nancy = Deck(image_files, 2, 3)
-cocos.director.director.init(resizable=True)
-wormcard = Cards(nancy.shuffled[0], blank_file)
-
-build_deck(image_files, 2,3)
 
 class Martha(cocos.layer.ColorLayer):
     is_event_handler = True
-    def __init__(self, deck):
+    def __init__(self, hand, cards):
         super( Martha, self).__init__(0,0,0,255)
         # Set up event handling from previous MouseDisplay() class
-        self.deck = deck
+        self.hand = hand
         self.posx = 100
         self.posy = 240
-        self.text = cocos.text.Label(font_size=18, x=10, y=10)# , color = (0,0,0,0))
+        self.text = cocos.text.Label(font_size=18, x=10, y=10 , color = (0,0,0,0))
         self.add( self.text )
 
         label = cocos.text.Label('Matching Martha',
             font_name='Courier',
             font_size=32,
             anchor_x='center', anchor_y='center')
-        label.position = 320,440
+        label.position = 320,650
         self.add( label )
 
-        positions = [(150,300), (300,300), (450,300), (150,150), (300,150), (450,150)]
-        for position, card in zip(positions, deck):
-            card.blank.position = position
-            card.image.position = position
+        self.score = cocos.text.Label(font_size=24, x=320, y=30)
+        self.add(self.score)
+
+        for posxy, card in zip(hand.posxy, cards):
+            card.blank.position = posxy
+            card.image.position = posxy
             self.add(card.blank)
             self.add(card.image)
 
     def update_text (self, x, y):
         in_sprite = False
-        for card in deck:
+        for card in cards:
             if card.blank.contains(x,y):
                 text = 'IN SPRITE'
                 self.text.element.text = text
@@ -90,20 +86,25 @@ class Martha(cocos.layer.ColorLayer):
             self.text.element.y = 10
 
     def on_mouse_motion (self, x, y, dx, dy):
-            self.update_text (x, y)
+        self.update_text (x, y)
+
 
     def on_mouse_press (self, x, y, buttons, modifiers):
-        for card in deck:
+        for card in cards:
             if card.blank.contains(x,y) == True :
                 card.image.opacity = 255
 
+
+
 if __name__ == "__main__":
-    cocos.director.director.init(resizable=True)
-    deck = [Cards(file, blank_file) for file in image_files] #replace with real deck via deck function
-    martha_layer = Martha(deck)
+    cocos.director.director.init(height = 690, width = 640)
+    hand = Hand(image_files, 3, 4)
+    cards = []
+    for i in hand.shuffled:
+        cards.append(Cards(i, blank_file))
+    martha_layer = Martha(hand, cards)
     main_scene = cocos.scene.Scene(martha_layer)
     cocos.director.director.run(main_scene)
-
 
 # Store images outside somewhere
 
